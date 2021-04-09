e5cf781f11640d0f8df9e49b9f396711377baf3b
David-3696 <81694814+David-3696@users.noreply.github.com>
2021-04-09 09:44:20 -0400

    Create DNA-converter.py

---
+++ DNA-converter.py
@@ -0,0 +1,42 @@
+
+print("____________________________________________________")
+
+print("          DNA to RNA to Protein Converter")
+
+print("____________________________________________________")
+DNA = input("Please enter DNA sequence: \n")
+print("____________________________________________________")
+print("DNA:", DNA,)
+
+RNA = DNA.maketrans("AGCT", "AGCU")
+
+rna = DNA.translate(RNA)
+
+print("RNA:", DNA.translate(RNA))
+
+RNA_Codons = {
+
+    "UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
+    "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
+    "UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+    "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+    "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+    "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+    "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+    "CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+    "AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+    "ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+    "AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+    "AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+    "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+    "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+    "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+    "GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",
+}
+
+
+protein = ""
+for i in range(0, len(rna), 3):
+    codon = rna[i:i+3]
+    protein += RNA_Codons[codon]
+print(f"Protein: {protein}")
